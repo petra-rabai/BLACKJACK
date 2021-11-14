295dd62da85b8cde4e9eadf56af519959d9c28c3
Petra Rábai <petra.rabai@icloud.com>
2021-11-14 13:30:55 +0100

    Add project files.

---
+++ BLACKJACK/IPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLACKJACK
+{
+    public interface IPlayer
+    {
+        int Score { get; set; }
+        string[] Cards { get; set; }
+        string[] Decision { get; set; }
+        void GetStartCards();
+    }
+}
